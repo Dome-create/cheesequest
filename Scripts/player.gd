92a9e3265dff0ef437e6181de5fa2fb10b9d62e9
karlyikdaniel-code <karlyikdaniel@gmail.com>
2026-01-12 20:24:03 +0100

    dash

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-
+var dash_direction = 0
 const WALK = 200
 const JUMP_VELOCITY = -800
 const SPRINT = 400
-const DASH_SPEED = 800        # Speed during dash
-const DASH_TIME = 0.2         # How long dash lasts (seconds)
+const DASH_SPEED = 1000       # Speed during dash
+const DASH_TIME = 0.2       # How long dash lasts (seconds)
 const DASH_COOLDOWN = 0.5     # Time before you can dash again
 
 var is_dashing = false
@@ -17,7 +17,7 @@ func _physics_process(delta: float) -> void:
 		get_tree().change_scene_to_file("res://Scenes/Main_Menu.tscn")
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += (get_gravity() * 1.5) * delta + Vector2(0, 20)
+		velocity += (get_gravity() * 1.3) * delta + Vector2(0, 20)
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -34,9 +34,15 @@ func _physics_process(delta: float) -> void:
 		is_dashing = true
 		dash_timer = DASH_TIME
 		dash_cooldown_timer = DASH_COOLDOWN
+		dash_direction = Input.get_axis("left", "right")
+
+	# fallback if no input (dash facing direction)
+	if dash_direction == 0:
+		dash_direction = sign($SandorExport.scale.x)
 	var direction := Input.get_axis("left", "right")
 	if is_dashing:
-		velocity.x = direction * DASH_SPEED
+		velocity.x = dash_direction * DASH_SPEED
+		velocity.y = 0
 		dash_timer -= delta
 		if dash_timer <= 0:
 			is_dashing = false
