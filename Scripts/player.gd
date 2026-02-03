276a70ae93b89a866317651f19f334a40d791e46
Dome-create <domekrajczar7@gmail.com>
2026-02-03 22:43:24 +0100

    Final sprint and cracker fix

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
 
 signal attack_state(attacking)
-signal coins_changed(coins: int)
 
 # BIG JUMP / CHARGE JUMP
 var is_charging_jump := false
@@ -20,10 +19,16 @@ var dash_direction = 0
 const WALK = 200
 const JUMP_VELOCITY = -800
 const SPRINT = 400
+var sprinting = false
 var smol= -400
 var hanyszo = 0
 
-#dash, wait is this a reference?!
+#dash, and sprint wait is this a reference?!
+var elapsed_sprint_time := 0
+@warning_ignore("unused_signal")
+signal explode
+var multiplier = 1
+var phase = [1.5, 2, 3,]
 var can_dash = true
 const DASH_SPEED = 1000       # Speed during dash
 const DASH_TIME = 0.2       # How long dash lasts (seconds)
@@ -31,15 +36,11 @@ const DASH_COOLDOWN = 0.5     # Time before you can dash again
 var is_dashing = false
 var dash_timer = 1
 var dash_cooldown_timer = 0.5
-var coins: int = 0
-
-func _ready() -> void:
-	emit_signal("coins_changed", coins)
 
 func _physics_process(delta: float) -> void:
 	# gravity.
 	if not is_on_floor():
-		velocity += (get_gravity() * 1.3) * delta + Vector2(0, 20)
+		velocity += (get_gravity() * 1.2) * delta + Vector2(0, 20)
 	# --- AIR → LAND DETECTION ---
 		was_in_air = true
 	elif was_in_air:
@@ -48,10 +49,13 @@ func _physics_process(delta: float) -> void:
 			is_charging_jump = true
 			velocity = Vector2.ZERO
 		was_in_air = false
-
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		@warning_ignore("integer_division")
+		velocity.y = JUMP_VELOCITY / 4
 	# --- NORMAL JUMP ---
 	if Input.is_action_just_pressed("jump") and is_on_floor() and not is_charging_jump:
 		velocity.y = JUMP_VELOCITY
+		
 	# --- BIG JUMP RELEASE ---
 	if is_charging_jump and Input.is_action_just_released("springy"):
 		is_charging_jump = false
@@ -104,9 +108,21 @@ func _physics_process(delta: float) -> void:
 			is_dashing = false
 	else:
 		if Input.is_action_pressed("sprint"):
-			velocity.x = direction * SPRINT
+			velocity.x = direction * SPRINT * multiplier
 		else:
 			velocity.x = direction * WALK
+			
+		if Input.is_action_just_pressed("sprint"):
+			sprinting = true
+			$Timer.start()
+		if Input.is_action_just_released("sprint"):
+			$Timer.stop()
+			sprinting=false
+			multiplier = 1
+
+	if sprinting== true:
+		speedingmouse()
+
 # Get the input direction and handle the movement/deceleration.
 	if Input.is_action_pressed("down") and Input.is_action_pressed("attack") and not is_on_floor():
 		velocity.y = 1400
@@ -114,7 +130,6 @@ func _physics_process(delta: float) -> void:
 	#sprite direction
 	if direction != 0:
 		$SandorExport.scale.x = direction * 0.5
-
 	#groundpound
 	move_and_slide()
 	
@@ -129,7 +144,15 @@ func start_attack():
 
 	is_attacking = false
 	emit_signal("attack_state", is_attacking)
-
-func add_coin(amount: int = 1) -> void:
-	coins += amount
-	emit_signal("coins_changed", coins)
+	
+func speedingmouse():
+	elapsed_sprint_time = $Timer.wait_time - $Timer.time_left
+	print("Elapsed sprint time: " + str(elapsed_sprint_time) + " sec, Multiplier: " + str(multiplier))
+	if elapsed_sprint_time == 4:
+		multiplier = phase[0]
+	elif elapsed_sprint_time == 8:
+		multiplier = phase[1]
+	elif elapsed_sprint_time == 12:
+		multiplier = phase[2]
+	elif elapsed_sprint_time == $Timer.wait_time:
+		emit_signal("explode")
